e56110d68a9cdad62f2cd31377760d6ecc545e15
Новиков Игорь <novikovio74@gmail.com>
2020-06-29 16:34:52 +0500

    Update dependencies and add babel decorator plugins for jest

--- src/config/jest/babel-transform.js
+++ src/config/jest/babel-transform.js
@@ -2,6 +2,15 @@ const babelJest = require("babel-jest");
 
 module.exports = babelJest.createTransformer({
   presets: [require.resolve("babel-preset-react-app")],
+  plugins: [
+    [
+      require.resolve("@babel/plugin-proposal-decorators"),
+      {
+        legacy: true,
+      },
+    ],
+    require.resolve("babel-plugin-parameter-decorator"),
+  ],
   babelrc: false,
-  configFile: false
+  configFile: false,
 });
